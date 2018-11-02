ee5fb514c0538b1977c04dea8b34645085f307a5
yuwzho <yuwzho@microsoft.com>
2018-11-02 13:47:32 +0800

    enable azds

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello from mywebapi');
+    res.send('Hello from webapi');
 });
 
-var port = 8080;
+var port = 80;
 var server = app.listen(port, function () {
     console.log('Listening on port ' + port);
 });
