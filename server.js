e88c6873c452e0139af861b2850a39b129634e93
yuwzho <yuwzho@microsoft.com>
2018-11-02 15:43:31 +0800

    init

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello from my webapi');
+    res.send('Hello from my web api');
 });
 
 var port = 8080;
